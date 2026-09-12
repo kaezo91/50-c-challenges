e5a3a6e3d5ba4576259b3e274be6de44ef83c399
kaezo91 <franpm3001@gmail.com>
2026-09-12 02:35:25 +0200

    segundo problema

---
+++ 2. responder.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int i;
+
+    cout << "Dime un numero: ";
+    cin >> i;
+
+    cout << "El numero es el " << i;    
+    return 0;
+}
